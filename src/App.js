5b5e6dcde3a55de4b2fa32f907a2ae8a0c41f709
vianeyAM <69022157+vianeyAM@users.noreply.github.com>
2020-10-27 01:24:21 +0000

    added WeatherIcons
    integrated with the API weather

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <div className="weather-app">
-          <Weather defaultCity="Dublin" />
+          <Weather defaultCity="Ireland" />
           <Forecast />
         </div>
       </header>
--- src/WeatherIcon.js
+++ src/WeatherIcon.js
@@ -1,15 +1,26 @@
 import React from "react";
 
-import * as Icons from "@intern0t/react-weather-icons";
-
+import "weather-icons/css/weather-icons.css";
 export default function WeatherIcon(props) {
   const codeMapping = {
-    "01d": "DaySunny",
-    "01n": "NightClear",
-    "02d": "DayCloudy",
-    "02n": "NightCloudy",
-    "03d": "Rain",
-    "03n": "Rain",
+    "01d": "wi-day-sunny",
+    "01n": "wi-night-clear",
+    "02d": "wi-day-cloudy",
+    "02n": "wi-night-alt-cloudy",
+    "03d": "wi-cloud",
+    "03n": "wi-cloud",
+    "04d": "wi-cloudy",
+    "04n": "wi-cloudy",
+    "09d": "wi-rain",
+    "09n": "wi-rain",
+    "10d": "wi-day-rain",
+    "10n": "wi-night-rain",
+    "11d": "wi-thunderstorm",
+    "11n": "wi-thunderstorm",
+    "13d": "wi-snow",
+    "13n": "wi-snow",
+    "50d": "wi-fog",
+    "50n": "wi-fog",
   };
-  return <Icons.{codeMapping[props.code]} color="#D685B1" size={150} />;
+  return <i className={`wi ${codeMapping[props.code]} display-1`} />;
 }
